1023bb4766a6c707706f25521553b5a1f3949b9d
RUMX69 <yoselrai116@gmail.com>
2025-07-29 14:35:48 +0600

    29 July

--- firstprogram.py
+++ firstprogram.py
@@ -1,2 +1,10 @@
 print("Hello world")
 print("bruhh that was easy")
+a=60
+b=9
+sum=a+b
+print(sum)
+print("The sum of ", a, "and", b, "is", sum)
+name=input("Enter your name: ")
+age=input("Enter your age: ")
+print("Hello", name, "you are", age, "years old.")
