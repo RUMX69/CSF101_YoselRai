ac17ccd04739dee9fb35ad28ab43dd02d4289247
RUMX69 <yoselrai116@gmail.com>
2025-08-06 17:38:04 +0600

    Exercises

---
+++ csf101-python_exercises/basics/type_conversion.py
@@ -0,0 +1,10 @@
+age = 18
+age_str = str(age)
+message = "I am " + age_str + "years old."
+print(message)
+
+num_str = "69"
+num_int = int(num_str)
+print(num_int)
+
+non_num_str = "Hello"
---
+++ csf101-python_exercises/data_structures/dictionaries.py
@@ -0,0 +1,15 @@
+name = "Yosel"
+age = 18
+height = 1.7
+is_student = True
+
+person_info = {
+    "name": name,
+    "age": age,
+    "height": height,
+    "is_student": is_student
+}
+print(person_info)
+
+person_info["favrite_color"] = "blue"
+print(person_info)
