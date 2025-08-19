7a668a49d605932333dd90199462b6b8b5b86d9c
RUMX69 <yoselrai116@gmail.com>
2025-08-19 21:43:55 +0600

    Pratical

--- csf101-python_exercises/pratiacl2/Iterative_implementation.py
+++ csf101-python_exercises/pratiacl2/Iterative_implementation.py
@@ -1,12 +1,12 @@
-def iterative(n):
-    if n == 0:
+def iterative(n): # Function to calculate Fibonacci number using iteration
+    if n == 0: # Base case for iteration
         return 0
     elif n == 1:
         return 1
 
-    a, b = 0, 1
-    for i in range(2, n + 1):
-        a, b = b, a + b
-    return b
+    a, b = 0, 1 # Initial values for Fibonacci sequence
+    for i in range(2, n + 1): # Iterate from 2 to n
+        a, b = b, a + b # Update values for next Fibonacci number
+    return b # Return the nth Fibonacci number
 
-print(iterative(int(input("Enter a number: "))))
+print(iterative(int(input("Enter a number: ")))) # Input a number to get its Fibonacci value
--- csf101-python_exercises/pratiacl2/Recursive_implementation.py
+++ csf101-python_exercises/pratiacl2/Recursive_implementation.py
@@ -1,7 +1,7 @@
-def recursive(n):
-    if n <= 1:
-        return n
+def recursive(n): # Function to calculate Fibonacci number using recursion
+    if n <= 1: # Base case for recursion
+        return n # If n is 0 or 1, return n
     else:
-        return recursive(n-1) + recursive(n-2)
+        return recursive(n-1) + recursive(n-2) # Recursive case to calculate Fibonacci number
 
-print(recursive(int(input("Enter a number: "))))
+print(recursive(int(input("Enter a number: ")))) # Input a number to get its Fibonacci value
